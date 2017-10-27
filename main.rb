c7d5f43421f335be3547d27c0de3259afed5bb69
bjwhip <bjwhip@gmail.com>
2017-10-27 12:05:50 -0600

    Input from user traslated to ship position

--- main.rb
+++ main.rb
@@ -1,14 +1,35 @@
-grid_data = 
+@enemy_ships =
   {
-  a: [0,0,0,0,0],
-  b: [0,0,0,0,0],
-  c: [0,0,0,0,0],
-  d: [0,0,1,0,0],
-  e: [0,0,0,0,0],
+    a: [0,0,0,0,0],
+    b: [0,0,0,0,0],
+    c: [0,0,0,0,0],
+    d: [0,0,1,1,1],
+    e: [0,0,0,0,0],
+  }
+@my_ships =
+  {
+    a: [1,0,0,0,0],
+    b: [1,0,0,0,0],
+    c: [1,0,0,0,0],
+    d: [0,0,0,0,0],
+    e: [0,0,0,0,0],
   }
 
-grid_data[:d][0] = 1
 
-p grid_data[:d][0]
+def attack
+  puts "Where would you like to attack?"
+  gun = gets.chomp!
+  column = (gun.downcase.split"")[0].to_sym
+  row = (gun.split"")[1].to_i
+  @enemy_ships[column][row]
 
 
+ if @enemy_ships[column][row] == 1
+   puts "Thats a HIT!!"
+ else @enemy
+  puts "not ready"
+end
+
+end
+
+attack

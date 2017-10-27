86f10c2ca7e6cb0df9630d3b06542da503781d87
Richi Ruiz <ruizrichard09@gmail.com>
2017-10-27 13:11:29 -0600

    done finished fanito whala

--- main.rb
+++ main.rb
@@ -11,9 +11,12 @@ grid_data[:d][0] = 1
 
 def display grid_data
   puts "  " + ((1..5).to_a.join(" "))
+  puts " +-+-+-+-+-+  " + "          ARTY AR MATEY!!!" + "   \u{1F48E}"
   grid_data.each do |key, array|
-    puts key.to_s + " " + (array.join(" "))
+    puts key.to_s + "|" + (array.join("|")) + "|" + "\u{2620}"
+    puts " +-+-+-+-+-+  " 
   end
+  puts " \u{2620}" + "  \u{2620}" + "  \u{2620}" + "  \u{2620}" 
 end
 
 display grid_data
